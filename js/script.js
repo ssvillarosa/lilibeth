2829b235a9acee06a6f444e93dd4710b764ef00f
Steven Villarosa <steven.s.villarosa@gmail.com>
2021-12-26 19:18:40 +0800

    fix ui in mobile screen and add a fade in animation.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// Config
 const SCROLL_OFFSET = -100;
+
+// Page scrolling
 const links = document.querySelectorAll("#navbar nav li a");
 links.forEach((link) => {
   link.addEventListener("click", scrollToView.bind(link));
@@ -12,3 +15,24 @@ function scrollToView(e) {
     v.getBoundingClientRect().top + window.scrollY + SCROLL_OFFSET;
   window.scrollTo({ top: scrollPos, behavior: "smooth" });
 }
+
+// Animation fade in
+const elems = document.querySelectorAll(".flex-item");
+elemObserver = new IntersectionObserver(onVisible, {
+  root: null,
+  threshold: 0.3,
+});
+
+function onVisible(entries) {
+  entries.forEach(function (entry) {
+    if (entry.isVisible) return;
+    if (entry.isIntersecting) {
+      entry.target.classList.remove("hidden");
+    }
+  });
+}
+
+elems.forEach((elem) => {
+  elemObserver.observe(elem);
+  elem.classList.add("hidden");
+});
